ae89d98f2a4f9d46609ff274016f315e9118cd49
Mikhail <dotdot2132@gmail.com>
2024-08-18 12:20:22 +0300

    add/ working on admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,30 +1,42 @@
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, NgIf, ReactiveFormsModule, AdminDashboardComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
 export class AdminComponent {
+  valid: boolean = false
   inpControl = new FormControl('', [
-    Validators.required,
-    Validators.minLength(4)
+    Validators.required
   ])
 
   constructor(){
-    console.log(this.inpControl.value); 
+    this.inpControl.statusChanges.subscribe((status) => {
+      // console.log(status);
+      // console.log(this.inpControl.valid);
+      
+    })
+
   }
 
   onLog(){
-    if(this.inpControl.value === 'admin'){
-      console.log('true');      
+    if(this.inpControl.value === 'admin' && this.inpControl.valid){
+      this.valid = true
     } else{
-      console.log('false');
-      
+      this.valid = false
+    }
+  }
+
+  onEnter(event: KeyboardEvent){
+    if(event.key === 'Enter'){
+      this.onLog()
     }
   }
   
